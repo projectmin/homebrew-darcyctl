ce5e907b961eae088014242486f3d6ba3cdb457f
neilotoole <neilotoole@apache.org>
2021-05-05 21:00:30 -0600

    Brew formula update for darcyctl version v0.11.4

--- Formula/darcyctl.rb
+++ Formula/darcyctl.rb
@@ -5,16 +5,16 @@
 class Darcyctl < Formula
   desc "CLI for Darcy"
   homepage "https://meetdarcy.com"
-  version "0.10.6"
+  version "0.11.4"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/projectmin/homebrew-darcyctl/releases/download/v0.10.6/darcyctl-macos-amd64.tar.gz"
-    sha256 "9085a05ed657b1f8d9028ce5198261d488ae9f4178d4b679f32b62392e881dfd"
+    url "https://github.com/projectmin/homebrew-darcyctl/releases/download/v0.11.4/darcyctl-macos-amd64.tar.gz"
+    sha256 "12c6160b5c238c7f22aa29e95be45f3171d50599f751ac7d971c6060c82edfac"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/projectmin/homebrew-darcyctl/releases/download/v0.10.6/darcyctl-linux-amd64.tar.gz"
-    sha256 "cb719c4d2dd4b4243ef15d848e311eccaa3cd5698a7124c088eab925ed40254f"
+    url "https://github.com/projectmin/homebrew-darcyctl/releases/download/v0.11.4/darcyctl-linux-amd64.tar.gz"
+    sha256 "1122e78dd2ff2a677631975a30530e31420fe564655fa3a1a8e291cfcd0ec689"
   end
 
   def install
